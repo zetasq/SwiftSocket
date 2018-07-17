7bee190b8e9db5307b7606a95b32e90a2b299f9d
Zhu Shengqi <shengqi@narvii.com>
2018-07-17 18:32:33 +0800

    Initial Commit

---
+++ SwiftSocket/SwiftSocket.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSocket.h
+//  SwiftSocket
+//
+//  Created by Zhu Shengqi on 17/7/2018.
+//  Copyright © 2018 pebkit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSocket.
+FOUNDATION_EXPORT double SwiftSocketVersionNumber;
+
+//! Project version string for SwiftSocket.
+FOUNDATION_EXPORT const unsigned char SwiftSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSocket/PublicHeader.h>
+
+
